b020e92ef68f292d22d87fa480985730af16cf65
mike <mike.godunoff2020@mail.ru>
2022-06-16 12:00:04 +0300

    sha2 added

--- lesson_22_3.rb
+++ lesson_22_3.rb
@@ -5,12 +5,15 @@ input_phrase = gets.to_s
 puts "Каким способом зашифровать?"
 puts "1.MD5"
 puts "2.SHA1"
+puts "3.SHA2"
 #choice and operations
 method_choice = gets.chomp
 puts "Вот что получилось:"
 
 if method_choice == 1
   puts Digest::MD5.hexdigest input_phrase
-else
+elsif method_choice == 2
   puts Digest::SHA1.hexdigest input_phrase
+else method_choice == 3
+  puts Digest::SHA2.hexdigest input_phrase
 end
